09a70d0d8629d7b8dba3e5e1d1566dee50b61362
sicris <sicris.embay@gmail.com>
2023-01-27 23:13:32 +0800

    + initialization of CAN transport + Tested XCP Connect and Disconnect

--- xcp_host/ui.Designer.cs
+++ xcp_host/ui.Designer.cs
@@ -28,20 +28,130 @@ namespace xcp_host
         /// </summary>
         private void InitializeComponent()
         {
+            this.cbb_channel = new System.Windows.Forms.ComboBox();
+            this.cbb_baudrates = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btn_HwRefresh = new System.Windows.Forms.Button();
+            this.btn_Connect = new System.Windows.Forms.Button();
+            this.textBox_BroadcastID = new System.Windows.Forms.TextBox();
+            this.textBox_MasterID = new System.Windows.Forms.TextBox();
+            this.textBox_SlaveID = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
+            // 
+            // cbb_channel
+            // 
+            this.cbb_channel.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbb_channel.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cbb_channel.Location = new System.Drawing.Point(32, 42);
+            this.cbb_channel.Name = "cbb_channel";
+            this.cbb_channel.Size = new System.Drawing.Size(119, 21);
+            this.cbb_channel.TabIndex = 37;
+            this.cbb_channel.SelectedIndexChanged += new System.EventHandler(this.cbb_channel_SelectedIndexChanged);
+            // 
+            // cbb_baudrates
+            // 
+            this.cbb_baudrates.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbb_baudrates.Location = new System.Drawing.Point(160, 42);
+            this.cbb_baudrates.Name = "cbb_baudrates";
+            this.cbb_baudrates.Size = new System.Drawing.Size(152, 21);
+            this.cbb_baudrates.TabIndex = 38;
+            this.cbb_baudrates.SelectedIndexChanged += new System.EventHandler(this.cbb_baudrates_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(29, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(56, 15);
+            this.label1.TabIndex = 42;
+            this.label1.Text = "Channel:";
+            // 
+            // label2
+            // 
+            this.label2.Location = new System.Drawing.Point(157, 24);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(56, 15);
+            this.label2.TabIndex = 43;
+            this.label2.Text = "Baudrate:";
+            // 
+            // btn_HwRefresh
+            // 
+            this.btn_HwRefresh.Cursor = System.Windows.Forms.Cursors.Default;
+            this.btn_HwRefresh.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.btn_HwRefresh.Location = new System.Drawing.Point(329, 31);
+            this.btn_HwRefresh.Name = "btn_HwRefresh";
+            this.btn_HwRefresh.Size = new System.Drawing.Size(65, 40);
+            this.btn_HwRefresh.TabIndex = 46;
+            this.btn_HwRefresh.Text = "Refresh";
+            this.btn_HwRefresh.Click += new System.EventHandler(this.btn_HwRefresh_Click);
+            // 
+            // btn_Connect
+            // 
+            this.btn_Connect.Location = new System.Drawing.Point(400, 33);
+            this.btn_Connect.Name = "btn_Connect";
+            this.btn_Connect.Size = new System.Drawing.Size(102, 37);
+            this.btn_Connect.TabIndex = 47;
+            this.btn_Connect.Text = "Connect";
+            this.btn_Connect.UseVisualStyleBackColor = true;
+            this.btn_Connect.Click += new System.EventHandler(this.btn_Connect_Click);
+            // 
+            // textBox_BroadcastID
+            // 
+            this.textBox_BroadcastID.Location = new System.Drawing.Point(119, 97);
+            this.textBox_BroadcastID.Name = "textBox_BroadcastID";
+            this.textBox_BroadcastID.Size = new System.Drawing.Size(75, 20);
+            this.textBox_BroadcastID.TabIndex = 48;
+            this.textBox_BroadcastID.Text = "300";
+            // 
+            // textBox_MasterID
+            // 
+            this.textBox_MasterID.Location = new System.Drawing.Point(119, 123);
+            this.textBox_MasterID.Name = "textBox_MasterID";
+            this.textBox_MasterID.Size = new System.Drawing.Size(75, 20);
+            this.textBox_MasterID.TabIndex = 48;
+            this.textBox_MasterID.Text = "555";
+            // 
+            // textBox_SlaveID
+            // 
+            this.textBox_SlaveID.Location = new System.Drawing.Point(119, 149);
+            this.textBox_SlaveID.Name = "textBox_SlaveID";
+            this.textBox_SlaveID.Size = new System.Drawing.Size(75, 20);
+            this.textBox_SlaveID.TabIndex = 48;
+            this.textBox_SlaveID.Text = "554";
             // 
             // ui
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox_SlaveID);
+            this.Controls.Add(this.textBox_MasterID);
+            this.Controls.Add(this.textBox_BroadcastID);
+            this.Controls.Add(this.btn_Connect);
+            this.Controls.Add(this.btn_HwRefresh);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.cbb_channel);
+            this.Controls.Add(this.cbb_baudrates);
             this.Name = "ui";
             this.Text = "Test User Interface";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.ui_FormClosing);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.ComboBox cbb_channel;
+        private System.Windows.Forms.ComboBox cbb_baudrates;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btn_HwRefresh;
+        private System.Windows.Forms.Button btn_Connect;
+        private System.Windows.Forms.TextBox textBox_BroadcastID;
+        private System.Windows.Forms.TextBox textBox_MasterID;
+        private System.Windows.Forms.TextBox textBox_SlaveID;
     }
 }
 
--- xcp_host/ui.cs
+++ xcp_host/ui.cs
@@ -7,14 +7,176 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Peak.Can.Xcp;
+using Peak.Can.Basic;
 
 namespace xcp_host
 {
+    using TXCPChannel = System.UInt16;
+    using TXCPHandle = System.UInt32;
+    using TPCANHandle = System.UInt16;
+
     public partial class ui : Form
     {
+        private TXCPChannel m_XcpChannel;
+        private TXCPHandle m_XcpSession;
+        private TXCPProtocolLayerConfig m_XcpProtocolConfig;
+        private TXCPTransportLayerCAN m_XcpSlaveData;
+        private TPCANHandle[] m_HandlesArray;
+        private TPCANHandle m_PcanHandle;
+        private TPCANBaudrate m_BaudRate;
+
+        #region Method
         public ui()
         {
             InitializeComponent();
+
+            this.m_HandlesArray = new TPCANHandle[] 
+            {
+                PCANBasic.PCAN_USBBUS1,
+                PCANBasic.PCAN_USBBUS2,
+                PCANBasic.PCAN_USBBUS3,
+                PCANBasic.PCAN_USBBUS4,
+                PCANBasic.PCAN_USBBUS5,
+                PCANBasic.PCAN_USBBUS6,
+                PCANBasic.PCAN_USBBUS7,
+                PCANBasic.PCAN_USBBUS8,
+            };
+            // Protocol timouts configuration: 
+            m_XcpProtocolConfig = new TXCPProtocolLayerConfig();
+            m_XcpProtocolConfig.T1 = 25;
+            m_XcpProtocolConfig.T2 = 25;
+            m_XcpProtocolConfig.T3 = 25;
+            m_XcpProtocolConfig.T4 = 25;
+            m_XcpProtocolConfig.T5 = 25;
+            m_XcpProtocolConfig.T6 = 25;
+            m_XcpProtocolConfig.T7 = 25;
+
+            m_XcpSession = 0;
+            m_XcpChannel = 0;
+
+            this.btn_HwRefresh_Click(this, new EventArgs());
+        }
+
+        #region Button Event Handlers
+        private void btn_HwRefresh_Click(object sender, EventArgs e)
+        {
+            UInt32 iBuffer;
+            TPCANStatus stsResult;
+
+            // Clears the Channel combioBox and fill it again with 
+            // the PCAN-Basic handles for no-Plug&Play hardware and
+            // the detected Plug&Play hardware
+            cbb_channel.Items.Clear();
+            cbb_baudrates.Items.Clear();
+            try {
+                for (int i = 0; i < m_HandlesArray.Length; i++) {
+                    // Includes all no-Plug&Play Handles
+                    if (( m_HandlesArray[i] >= PCANBasic.PCAN_USBBUS1 ) &&
+                        ( m_HandlesArray[i] <= PCANBasic.PCAN_USBBUS8 )) {
+                        stsResult = PCANBasic.GetValue(m_HandlesArray[i], TPCANParameter.PCAN_CHANNEL_CONDITION, out iBuffer, sizeof(UInt32));
+                        if (( stsResult == TPCANStatus.PCAN_ERROR_OK ) && ( iBuffer == PCANBasic.PCAN_CHANNEL_AVAILABLE )) {
+                            cbb_channel.Items.Add(string.Format("PCAN-USB{0}", m_HandlesArray[i] & 0x0F));
+                        }
+                    }
+                }
+                cbb_channel.SelectedIndex = cbb_channel.Items.Count - 1;
+            } catch (DllNotFoundException) {
+                MessageBox.Show("Unable to find the library: PCANBasic.dll !", "Error!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                Environment.Exit(-1);
+            }
+            cbb_baudrates.Items.Clear();
+            string[] baudName = Enum.GetNames(typeof(TPCANBaudrate));
+            for (int i = 0; i < baudName.Length; i++) {
+                cbb_baudrates.Items.Add(baudName[i]);
+            }
+            cbb_baudrates.SelectedIndex = 3;
+        }
+
+        private void btn_Connect_Click(object sender, EventArgs e)
+        {
+            try {
+                if(btn_Connect.Text == "Connect") {
+                    TXCPResult result;
+                    StringBuilder strText = new StringBuilder(256);
+
+                    this.m_XcpSlaveData.BroadcastID = UInt32.Parse(this.textBox_BroadcastID.Text, System.Globalization.NumberStyles.HexNumber);
+                    this.m_XcpSlaveData.MasterID = UInt32.Parse(this.textBox_MasterID.Text, System.Globalization.NumberStyles.HexNumber);
+                    this.m_XcpSlaveData.SlaveID = UInt32.Parse(this.textBox_SlaveID.Text, System.Globalization.NumberStyles.HexNumber);
+                    result = XCPApi.InitializeCanChannel(out this.m_XcpChannel, this.m_PcanHandle, this.m_BaudRate);
+                    if(result == TXCPResult.XCP_ERR_OK) {
+                        // Associates a CAN channel with Slave data to communicate over CAN
+                        // This operation retrieves the Session-handle (m_XcpSession) to be 
+                        // used with all other XCP Command-Functions
+                        result = XCPApi.AddSlaveOnCAN(this.m_XcpChannel, this.m_XcpSlaveData, this.m_XcpProtocolConfig, out this.m_XcpSession);
+                        if (result == TXCPResult.XCP_ERR_OK) {
+                            byte[] msg;
+                            msg = new byte[Peak.Can.Xcp.XCPApi.CAN_MAX_LEN];
+                            // Enables a session (connection) between this (master) application 
+                            // and the slave described by the m_XcpSession handle
+                            result = XCPApi.Connect(m_XcpSession, 0, msg, (ushort)msg.Length);
+                            if(result == TXCPResult.XCP_ERR_OK) {
+                                btn_Connect.Text = "Disconnect";
+                            }
+                        } else {
+                            XCPApi.GetErrorText(result, strText);
+                            MessageBox.Show("Error: " + strText.ToString());
+                            XCPApi.UninitializeChannel(m_XcpChannel);
+                            m_XcpChannel = 0;
+                        }
+                    } else {
+                        XCPApi.GetErrorText(result, strText);
+                        MessageBox.Show("Error: " + strText.ToString());
+                    }
+                } else {
+                    TXCPResult result;
+                    StringBuilder strText = new StringBuilder(256);
+                    byte[] msg;
+                    msg = new byte[Peak.Can.Xcp.XCPApi.CAN_MAX_LEN];
+                    // Sets the ECU (slave) in a disconnected state
+                    result = XCPApi.Disconnect(this.m_XcpSession, msg, (ushort)msg.Length);
+                    if(result != TXCPResult.XCP_ERR_OK) {
+                        XCPApi.GetErrorText(result, strText);
+                        MessageBox.Show("Error: " + strText.ToString());
+                    }
+                    result = XCPApi.UninitializeChannel(this.m_XcpChannel);
+                    if (result != TXCPResult.XCP_ERR_OK) {
+                        XCPApi.GetErrorText(result, strText);
+                        MessageBox.Show("Error: " + strText.ToString());
+                    }
+                    btn_Connect.Text = "Connect";
+                }
+            } catch (Exception ex) {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        #endregion // Button Event Handlers
+
+        #region ComboBox Event Handlers
+        private void cbb_channel_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string strTemp = cbb_channel.Text;
+            strTemp = strTemp.Substring(strTemp.IndexOf('B') + 1, 1);
+            strTemp = "0x5" + strTemp;
+            this.m_PcanHandle = Convert.ToByte(strTemp, 16);
+        }
+
+        private void cbb_baudrates_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            TPCANBaudrate[] baudValue = (TPCANBaudrate[])Enum.GetValues(typeof(TPCANBaudrate));
+            this.m_BaudRate = baudValue[cbb_baudrates.SelectedIndex];
+        }
+        #endregion // ComboBox Event Handlers
+
+        #endregion // Method
+
+        private void ui_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            /* Disconnect when closing */
+            if(this.btn_Connect.Text != "Connect") {
+                this.btn_Connect_Click(this, new EventArgs());
+            }
         }
     }
 }
